8c66f7ac47bba7c0f9babe6426de3e630676c8f2
Allan Salandanan <Allan@Moniques-MacBook-Pro.local>
2017-03-18 12:21:55 -0700

    Updated js and html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,3 +7,35 @@ apiKey: "AIzaSyAuYP04nQL20GAouCjVi0e47yMf_VFaHkc",
 	messagingSenderId: "490424550879"
 };
 firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// imgur API
+$("#imgur-submit").on("click", function () {
+	
+	var imgQuery = $("#imgur-search").val();
+	var imgurUrl = "";
+	
+	$.ajax({
+		url: imgurUrl,
+		method: "GET"
+	}).done(function (image) {
+		// save picture into database
+	})
+
+})
+
+// youtube API
+$("#youtube-submit").on("click", function () {
+
+	var youtubeQuery = $("#youtube-search").val();
+	var youtubeUrl = "";
+
+	$.ajax({
+		url: youtubeUrl,
+		method: "GET"
+	}).done(function (video) {
+		// save video into database
+	})
+	
+})
