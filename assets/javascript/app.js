028942b32aa7a506e6b9eb5eca826686caa18c76
Allan Salandanan <Allan@Moniques-MacBook-Pro.local>
2017-03-21 18:24:38 -0700

    Updated js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,17 @@
+// Set up YouTube Data API
+var youtubeKey = "AIzaSyAuYP04nQL20GAouCjVi0e47yMf_VFaHkc";
+
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAuYP04nQL20GAouCjVi0e47yMf_VFaHkc",
+	authDomain: "project1-c9b34.firebaseapp.com",
+	databaseURL: "https://project1-c9b34.firebaseio.com",
+	storageBucket: "project1-c9b34.appspot.com",
+	messagingSenderId: "490424550879"
+};
+firebase.initializeApp(config);
+var ref = firebase.database().ref();
+
 $(document).ready(function(){
 
 $("#edit-area").hide();
