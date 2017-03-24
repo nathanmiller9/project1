d81eccdf35f38ed6813992acc3a8f5a7c9b0fe0b
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-03-23 21:57:09 -0700

    merge video search/play and notes functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,6 +12,20 @@ var config = {
 firebase.initializeApp(config);
 var ref = firebase.database().ref();
 
+//load iFrame/embedded video player
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
 
 $(document).ready(function(){
 
@@ -24,8 +38,7 @@ $("#start-button").on("click", function () {
 	$("#sample").hide();
 	$("#spacer").hide();
 	$("#edit-area").show();
-})
-
+});
 
 $("#recipient-input-btn").on("click", function (e) {
 	e.preventDefault();
@@ -48,15 +61,81 @@ $("#recipient-input-btn").on("click", function (e) {
 // Push songs into firebase
 $("#search-button").on("click", function (e) {
 	e.preventDefault();
-	ref.child("playlist").push({
-		artist: $("#artist-query").val(),
-		song: $("#song-query").val()
-	})	
-})
+	
+	$.ajax({
+		cache: false,
+		data: $.extend({
+			key: youtubeKey,
+			q: $("#artist-query").val() + " " + $("#song-query").val(),
+			part: "snippet"
+		}, {maxResults: 1, type: "video", videoEmbeddable: "true", videoSyndicated: "true"}),
+		dataType: "json",
+		type: "GET",
+		url: "https://www.googleapis.com/youtube/v3/search"
+	}).done(function(response) {
+		var str = JSON.stringify(response);
+		console.log(response);
+		var videoId = response.items[0].id.videoId;
+		var videoTitle = response.items[0].snippet.title;
+		var videoURL = "https://youtube.com/watch?v=" + videoId;
+
+		//show and play YouTube video result
+		$("#result-video-container").show();
+		player = new YT.Player('player', {
+		  height: '390',
+		  width: '640',
+		  videoId: videoId,
+		  events: {
+		    'onReady': onPlayerReady
+		  }
+		});
+
+		//add song object to playlist in firebase
+		ref.child("playlist").push({
+			artist: $("#artist-query").val(),
+			song: $("#song-query").val(),
+			videoId: videoId
+		});
+		
+		//Load song onto "Selected Song" panel
+		$("#active-song-container").show();
+		$("#active-song-title").text($("#song-query").val() + " - " + $("#artist-query").val());
+		
+		//DELETE EXISTING NOTES FROM SOME OTHER SONG
+		$("#active-song-notes").empty();
+
+		// get key of recently added song
+		var songArray = [];
+
+		ref.child("playlist").once("value", function(snapshot) {
+			snapshot.forEach(function(childSnapshot) {
+				songArray.push(childSnapshot.key);
+			});
+		});
+
+		var latestFirebaseKey = songArray[songArray.length - 1];
+
+		//add data-key to "Add Note" button in "Selected Song"
+		$("#active-song-add-button").attr("data-firebase-key", latestFirebaseKey);
+
+		//add notes for song
+		ref.child("playlist").child(latestFirebaseKey).child("notes").once("value", function(snapshot) {
+			snapshot.forEach(function(childSnapshot) {
+				var content = childSnapshot.val().content;
+				var newDiv = $("<div class='note'>").text(content);
+				$("#active-song-notes").append(newDiv);
+			});
+		});
+
+		//clear search values
+		$("#artist-query").val("");
+		$("#song-query").val("");
+	});
+});
 
 //Displays current song list 
 ref.child("playlist").on("child_added" , function (songItem) {
-		var songList = $("<li>")
+		var songList = $("<li>");
 		var songAndArtist;
 		var songKey = songItem.getKey();
 		songAndArtist = songItem.val().artist + " - " + songItem.val().song;
@@ -67,7 +146,7 @@ ref.child("playlist").on("child_added" , function (songItem) {
 		$("#mixtape-container").append(songList);
 
 		console.log(songList);
-	})
+});
 
 // Disable search button 
 ref.child("playlist").on("value", function(children) {
@@ -78,7 +157,7 @@ ref.child("playlist").on("value", function(children) {
 		else {
 			$("#search-button").prop("disabled", false);
 		}
-})
+});
 
 // Remove song from playlist button
 $(document.body).on("click", ".checkbox", function() {
@@ -88,7 +167,7 @@ $(document.body).on("click", ".checkbox", function() {
 
 	//remove from firebase
 	ref.child("playlist").child(remKey).remove();
-})
+});
 
 // imgur API
 $("#imgur-submit").on("click", function () {
@@ -103,7 +182,7 @@ $("#imgur-submit").on("click", function () {
 		// save picture into database
 	})
 
-})
+});
 
 // youtube API
 $("#youtube-submit").on("click", function () {
@@ -118,4 +197,23 @@ $("#youtube-submit").on("click", function () {
 		// save video into database
 	})
 
-})
+});
+
+//Add a note to selected song
+$("#active-song-add-button").on("click", function(e) {
+	e.preventDefault();
+	var songKey = $(this).attr("data-firebase-key");
+	var noteContent = $("#active-song-textarea").val().trim();
+	if(noteContent !== "") {
+		ref.child("playlist").child(songKey).child("notes").push({
+			content: noteContent
+		});
+	}
+
+	//add new notes to active song note area
+	var newNoteContent = $("#active-song-textarea").val();
+	var newDiv = $("<div class='note'>").text(newNoteContent);
+	$("#active-song-notes").append(newDiv);
+
+	$("#active-song-textarea").val("");
+});
